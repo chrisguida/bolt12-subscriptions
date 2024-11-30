a1e9e2e41f4d6d0c139251049563785f83374080
crguzdu <crguzman@protonmail.com>
2024-11-30 17:12:30 -0600

    add subscriptiongenerate rpc method

--- subscription.py
+++ subscription.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
 from pyln.client import Plugin
+import json
 
 plugin = Plugin()
 
 @plugin.init()
 def init(options, configuration, plugin, **kwargs):
     plugin.log("Plugin subscription.py initialized")
+
+@plugin.method("subscriptiongenerate")
+def subscriptiongenerate(plugin ):
+    offer = plugin.rpc.offer("any")
+    plugin.log(str(offer))
+
+    offer_obj = {offer["bolt12"]: None}
+
+    # Add entries.
+    plugin.rpc.datastore(key="bolt12-subscriptions", string=json.dumps(offer_obj), mode="create-or-replace")
 
 @plugin.hook("onion_message_recv")
 def onion_message_recv(onion, htlc, plugin, **kwargs):
